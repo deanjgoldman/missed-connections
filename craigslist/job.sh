122fff7bff544c9ba1c83c1705006f50ab22bd8b
dgoldman916 <deanjgoldman@gmail.com>
2018-05-21 16:05:48 -0400

    job.sh

---
+++ craigslist/job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Today's date
+DATE=$(date +%Y-%m-%d)
+# word count content
+wc -l craigslist_content.txt
+# initialize new file
+touch links_${DATE}.txt
+# Uncomment links pipeline 
+sed -i '' '75 s/^#//' settings.py
+# Comment content pipeline
+sed -i '' '76 s/^/#/' settings.py
+# Run links crawl
+echo "Running links crawl"
+scrapy crawl links    
+# Comment links
+sed -i '' '75 s/^/#/' settings.py
+# Comment content pipeline
+sed -i '' '76 s/^#//' settings.py
+# Run content crawl
+scrapy crawl content 
+# Concat content to master 
+echo craigslist_${DATE}.txt >> craigslist_content.txt
