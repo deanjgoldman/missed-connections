fc04012b27bbcdd483c22f8cd6e6673719f51190
dgoldman916 <deanjgoldman@gmail.com>
2018-05-22 13:00:12 -0400

    connection config

--- craigslist/psql.py
+++ craigslist/psql.py
@@ -3,7 +3,7 @@ import psycopg2
 
 #USER = os.environ.get("USER")
 DATABASE_URL = os.environ.get("DATABASE_URL")
-conn = psycopg2.connect(dbname=DATABASE_URL, sslmode="require")
+conn = psycopg2.connect(dbname=DATABASE_URL, sslmode="require", port=5342)
 #cursor = conn.cursor()
 #cursor.execute("SELECT url FROM links;")
 #test = cursor.fetchall()
