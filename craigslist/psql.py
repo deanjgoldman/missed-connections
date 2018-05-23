1cd61eed4bea6d2b7b34b5e1b56599cf68e234d4
dgoldman916 <deanjgoldman@gmail.com>
2018-05-23 18:24:33 -0400

    Update psql.py

--- craigslist/psql.py
+++ craigslist/psql.py
@@ -1,11 +1,6 @@
 import os
 import psycopg2
 
-USER = os.environ.get("USER")
+
 DATABASE_URL = os.environ.get("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL)
-#cursor = conn.cursor()
-#cursor.execute("SELECT url FROM links;")
-#test = cursor.fetchall()
-#for row in test:
-#    print(row)
