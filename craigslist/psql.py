65af8203bd8aee946f6656c6fe76f06feca54290
dgoldman916 <deanjgoldman@gmail.com>
2018-06-07 07:54:21 -0400

    clean db connection script

--- craigslist/psql.py
+++ craigslist/psql.py
@@ -1,11 +1,8 @@
 import os
 import psycopg2
 
+# Setup db connection here
 USER = os.environ.get("USER")
 DATABASE_URL = os.environ.get("DATABASE_URL")
-conn = psycopg2.connect(DATABASE_URL)
-#cursor = conn.cursor()
-#cursor.execute("SELECT url FROM links;")
-#test = cursor.fetchall()
-#for row in test:
-#    print(row)
+conn = psycopg2.connect(dbname=DATABASE_URL)
+
