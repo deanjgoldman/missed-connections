ece588196a2dc259bd6f32268c1f837b3104b9c4
dgoldman916 <deanjgoldman@gmail.com>
2018-06-07 07:52:25 -0400

    cronjob cmds: cd into dir, setup env, db migration

--- craigslist/job.sh
+++ craigslist/job.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+# ------------------------------
+# jobs for MissedConnections
+# ------------------------------
+
+# Enter environment
+cd /Users/deangoldman/craigslist/craigslist
+source env/bin/activate
+# set up environment variables:
+# export LOCAL_DATABASE_URL="<LOCAL DATABASE>"
+bash setup.sh
 
 # Uncomment links pipeline 
 sed -i '' '75 s/^#//' settings.py
 # Comment content pipeline
 sed -i '' '76 s/^/#/' settings.py
 # Run links crawl
+echo 'running scrapy links'
 scrapy crawl links    
 # Comment links
 sed -i '' '75 s/^/#/' settings.py
 # Comment content pipeline
 sed -i '' '76 s/^#//' settings.py
 # Run content crawl
+echo 'running scrapy content'
 scrapy crawl content 
-
-# Add to github
-#git add logfile.txt
-#git commit -m "update ${DATE}"
-#git pull origin master
-#git push origin master
-#git push heroku master
+# migrate local db to app env db
+heroku pg:reset DATABASE_URL --confirm
+heroku pg:push LOCAL_DATABASE_URL
